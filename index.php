7f20e7e3cfc7a366c5ebf75c5245d9e236e73db3
ImNizamudin <nisamudin0407@gmail.com>
2022-06-13 21:07:17 +0700

    mengubah file index.php

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
 <body>
 
     <h1>Hello World!</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio culpa error accusamus non, tempora alias neque ipsa vero? Iusto ex iure accusantium at! Pariatur deleniti id quaerat quo autem consequuntur.</p>
     
 </body>
 </html>
